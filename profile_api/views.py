86f0dcd927ecb989688ad0f9fbc8f673ea865c3b
sksho <pop.shofi@mail.com>
2019-09-16 01:20:55 +0600

    login_api endpoint added

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, filters
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profile_api import serializers, models, permissions
 
@@ -109,3 +111,11 @@ class UserProfileVIewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email')
+
+
+class UserLoginAPIView(ObtainAuthToken):
+    """Handle creating user authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
